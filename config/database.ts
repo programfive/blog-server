f09d5fdb3ce96e75c958ea2f19f5dfcfc8122b27
josue david kennedy <104701497+programfive@users.noreply.github.com>
2025-04-12 10:54:28 -0400

    update changes

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ep-noisy-tree-acqv2c5r-pooler.sa-east-1.aws.neon.tech'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'database'),
-      user: env('DATABASE_USERNAME', 'database_owner'),
-      password: env('DATABASE_PASSWORD', 'npg_v1yEkLM5CJGt'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: {
         rejectUnauthorized: false
       },
